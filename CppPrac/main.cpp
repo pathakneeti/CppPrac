e7910d18bba0b7b38db49f014e95cd34c4be320d
Neeti <pathak.neetin@gmail.com>
2014-01-28 20:26:18 -0500

    notes

--- CppPrac/main.cpp
+++ CppPrac/main.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 
 int main(){
+    /*
+     
+     MINI LESSON ON NEW - DELETE 
+    heap is for new-delete and if you dont delete you risk memory leak
+    stack is if you dont use new, the object automatically gets destroyed when main is done. when you are out of scope. here you risk stack overflow if you make too many strings hello for example. 
+     
+    note: not that this will always be so noticeable, BUT memory allocation and deallocation on the heap take resources and time. there is that overhead when you use new-delete. people suggest that you use this wisely. basically whenever you want to dynamically allocate memory, and when you want to reference the object outside scope 
+     
+    */
+    
     //Cypher cc (5);
     Cypher * c = new Cypher(5);
     //pointerToC = &c;
